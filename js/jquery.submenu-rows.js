356f8ce0a244fce10602fd59983c7032132d3f20
Anko Anchev <anko.anchev@1stonlinesolutions.com>
2015-09-08 14:53:20 +0300

    Plugin Submenu-Rows: Remove app-specific code

--- js/jquery.submenu-rows.js
+++ js/jquery.submenu-rows.js
@@ -255,31 +255,3 @@ options:
     };
  
 }(jQuery));
-
-
-/* FIXME !!! NOT SUPPOSED TO BE HERE !!!*/
-//dynamically generated
-(function($) {
-    $("nav ul:topmost").extractSubmenus({
-        onPrepare: function(container) {
-            $(container).appendTo("#header");
-            container.id = "header_nav_submenus";
-
-            $("#header").mouseleave(function() {
-                $(container).find(".submenu:topmost").slideUp();
-                console.log("close");
-            });
-        },
-        onShow: function(event, target) {
-            $(target).slideDown();
-            return false;
-        },
-        onHide: function(event, target) {
-            if ($(target).is(".submenu .submenu")) {
-                console.log("hide");
-                $(target).slideUp();
-            }
-            return false;
-        }
-    });
-}(jQuery));
